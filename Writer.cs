b412efe60a55d44504491cc8aaf3c610420e162e
さる。 鬼百 <oni100@live.jp>
2020-05-28 10:33:58 +0900

    test

---
+++ Writer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Test
+{
+    public class Writer
+    {
+        private static Window mw; 
+
+        public void Write()
+        {
+        }
+
+        public void gg()
+        {
+            mw = (MainWindow)App.Current.MainWindow;
+            //MainWindow.TextBlockWrite("テスト");
+            var mainWindow = (MainWindow)App.Current.MainWindow;
+            var wm = Application.Current.MainWindow;
+            mainWindow.TextBlockWrite(mainWindow+"テストtest200528"+wm+" --- "+mw);
+           
+        }
+    }
+}
